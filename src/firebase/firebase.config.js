a28370f17c7cc6a69fb78bc3078a05547bafbc54
roufhasane <roufhasan5@gmail.comm>
2023-05-03 20:13:07 +0600

    firebase config added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAZgRB6unulECW4wESr_L2VvZ32faz0bw",
+  authDomain: "desi-food-hut.firebaseapp.com",
+  projectId: "desi-food-hut",
+  storageBucket: "desi-food-hut.appspot.com",
+  messagingSenderId: "152265813162",
+  appId: "1:152265813162:web:08780203f2a6bf35dc34d3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
